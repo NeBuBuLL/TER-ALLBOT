201788daa2ace4944d592b1b1b6289cbeddbb21a
NeBuBuLL <marrosebastien@protonmail.com>
2021-06-14 23:44:12 +0200

    Change function _step to step (valid fix ?)

--- MuJoCo/Simulation/dmbrl/env/robot.py
+++ MuJoCo/Simulation/dmbrl/env/robot.py
@@ -13,13 +13,14 @@ class RobotEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
         self.prev_qpos = None
         dir_path = os.path.dirname(os.path.realpath(__file__))
+        print(dir_path)
         mujoco_env.MujocoEnv.__init__(self, '%s/assets/robot.xml' % dir_path, 5)
         utils.EzPickle.__init__(self)
 
-    def _step(self, action):
+    def step(self, action):
         self.prev_qpos = np.copy(self.model.data.qpos.flat)
         self.do_simulation(action, self.frame_skip)
-        ob = self._get_obs()
+        ob = self.get_obs()
 
         reward_ctrl = -0.1 * np.square(action).sum()
         reward_run = ob[0] - 0.0 * np.square(ob[2])
@@ -28,7 +29,7 @@ class RobotEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         done = False
         return ob, reward, done, {}
 
-    def _get_obs(self):
+    def get_obs(self):
         return np.concatenate([
             (self.model.data.qpos.flat[:1] - self.prev_qpos[:1]) / self.dt,
             self.model.data.qpos.flat[1:],
@@ -40,7 +41,7 @@ class RobotEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         qvel = self.init_qvel + np.random.normal(loc=0, scale=0.001, size=self.model.nv)
         self.set_state(qpos, qvel)
         self.prev_qpos = np.copy(self.model.data.qpos.flat)
-        return self._get_obs()
+        return self.get_obs()
 
     def viewer_setup(self):
         self.viewer.cam.distance = self.model.stat.extent * 0.25
